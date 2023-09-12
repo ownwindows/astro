188c6048a82e12418d2fc3c64157c565ac37336c
Marinus Eigenraam <marinus.eigenraam@finalist.nl>
2023-09-12 14:30:30 +0200

    corrections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
-import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), image()]
 });
